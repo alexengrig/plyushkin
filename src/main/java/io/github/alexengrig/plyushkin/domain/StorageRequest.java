e98e57958692d585c7766121562a483345372086
Grig Alex <alexengrigdev@gmail.com>
2019-08-08 20:17:13 +0300

    Create storage request and response

--- src/main/java/io/github/alexengrig/plyushkin/domain/StorageRequest.java
+++ src/main/java/io/github/alexengrig/plyushkin/domain/StorageRequest.java
@@ -16,26 +16,6 @@
 
 package io.github.alexengrig.plyushkin.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-/**
- * <p>The thing contains a value.</p>
- *
- * @param <T> the content value type
- * @author Grig Alex
- * @version 0.1.0
- * @since 0.1.0
- */
-@Data
-@AllArgsConstructor
-public class Thing<T> {
-    /**
-     * <p>The content value.</p>
-     *
-     * @version 0.1.0
-     * @see T
-     * @since 0.1.0
-     */
-    private final T value;
+public interface StorageRequest<T> {
+    T getContent();
 }
