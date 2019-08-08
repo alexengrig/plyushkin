323cb3750e0ee5dd4eff2b6cbb41b36e8e8a9c48
Grig Alex <alexengrigdev@gmail.com>
2019-08-08 20:24:11 +0300

    Create multipart file storage service

--- src/main/java/io/github/alexengrig/plyushkin/service/StorageService.java
+++ src/main/java/io/github/alexengrig/plyushkin/service/StorageService.java
@@ -16,44 +16,11 @@
 
 package io.github.alexengrig.plyushkin.service;
 
-import java.util.function.Function;
+import io.github.alexengrig.plyushkin.domain.StorageRequest;
+import io.github.alexengrig.plyushkin.domain.StorageResponse;
 
-/**
- * <p>Represents a service-function that accepts a thing and produces a result.</p>
- *
- * <p>This is a <a href="package-summary.html">functional interface</a>
- * whose functional method is {@link #store(Object)}.
- *
- * @param <T> the type of the input to the storage service
- * @param <R> the type of the result of the storage service
- * @author Grig Alex
- * @version 0.1.0
- * @see java.util.function.Function
- * @since 0.1.0
- */
-@FunctionalInterface
-public interface StorageService<T, R> extends Function<T, R> {
-    /**
-     * <p>The store method.</p>
-     *
-     * @param thing a thing
-     * @return a result
-     * @version 0.1.0
-     * @since 0.1.0
-     */
-    R store(T thing);
+public interface StorageService<T> {
+    StorageResponse<T> store(StorageRequest<T> request);
 
-    /**
-     * <p>The store method.</p>
-     *
-     * @param thing a thing
-     * @return a result
-     * @version 0.1.0
-     * @see java.util.function.Function#apply(Object)
-     * @since 0.1.0
-     */
-    @Override
-    default R apply(T thing) {
-        return store(thing);
-    }
+    StorageResponse<T> get(StorageRequest<T> request);
 }
