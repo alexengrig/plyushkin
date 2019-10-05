87c54657469e549da10bbfaecbbc6a8857b8c7c7
Grig Alex <alexengrigdev@gmail.com>
2019-10-05 22:03:58 +0300

    Create FileController

---
+++ src/main/java/io/github/alexengrig/plyushkin/controller/FileController.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.alexengrig.plyushkin.controller;
+
+import io.github.alexengrig.plyushkin.service.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("files")
+@RequiredArgsConstructor
+public class FileController {
+    private final FileService service;
+
+    @GetMapping("/{fileId}")
+    public ResponseEntity<?> getById(@PathVariable Long fileId) {
+        return service.getById(fileId)
+                .map(ResponseEntity::ok)
+                .orElseGet(ResponseEntity.notFound()::build);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(MultipartFile file) {
+        return ResponseEntity.ok(service.save(file));
+    }
+}
