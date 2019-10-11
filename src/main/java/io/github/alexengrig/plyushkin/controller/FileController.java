3d1e9b1a9d9893e49eb4f02d38744a2354b7d048
Grig Alex <alexengrigdev@gmail.com>
2019-10-11 21:31:11 +0300

    Refactor save method to file controller

--- src/main/java/io/github/alexengrig/plyushkin/controller/FileController.java
+++ src/main/java/io/github/alexengrig/plyushkin/controller/FileController.java
@@ -24,12 +24,18 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URI;
 
 @RestController
 @RequestMapping("files")
 @RequiredArgsConstructor
 public class FileController {
     private final FileService service;
+
+    @PostMapping
+    public ResponseEntity<?> save(MultipartFile file) throws IOException {
+        return ResponseEntity.created(URI.create("/files/" + service.save(file).getId())).build();
+    }
 
     @GetMapping("/{fileId}")
     public ResponseEntity<?> getById(@PathVariable Long fileId) {
@@ -38,14 +44,9 @@ public class FileController {
                 .orElseGet(ResponseEntity.notFound()::build);
     }
 
-    @PostMapping
-    public ResponseEntity<?> save(MultipartFile file) throws IOException {
-        return ResponseEntity.ok(service.save(file));
-    }
-
     @GetMapping(value = "/{fileId}/raw")
     public @ResponseBody
-    ResponseEntity<byte[]> getRaw(@PathVariable Long fileId) throws IOException {
+    ResponseEntity<byte[]> getRawById(@PathVariable Long fileId) throws IOException {
         return service.getRawById(fileId)
                 .map(ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM)::body)
                 .orElseGet(ResponseEntity.notFound()::build);
